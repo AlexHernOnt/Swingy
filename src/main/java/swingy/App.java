2af5b03ea2b0ee0f0b93bc09313a68b40606cec6
Alejandro Hernandez ontoria <ahernand@c2r13s6.42madrid.com>
2024-11-01 20:00:47 +0100

    start question

--- src/main/java/swingy/App.java
+++ src/main/java/swingy/App.java
@@ -1,13 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   App.java                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahernand <ahernand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 18:54:10 by ahernand          #+#    #+#             */
+/*   Updated: 2024/11/01 19:59:47 by ahernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 package swingy;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+/*
+** Hello world!
+*/
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		hero h1;
+		String inputName = "";
+
+		System.err.println("Welcome to The Brighest Night");
+		Scanner scanner = new Scanner(System.in);
+		while (inputName.isEmpty()) {
+			if (inputName.isEmpty())
+				System.out.println("Write your hero's name:");
+				inputName = scanner.nextLine();
+		}
+		System.out.println("The hero name is '" + inputName + "'");
+
+
+	}
 }
---
+++ src/main/java/swingy/hero.java
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hero.java                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahernand <ahernand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 18:45:26 by ahernand          #+#    #+#             */
+/*   Updated: 2024/11/01 20:00:07 by ahernand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+package swingy;
+
+public class hero {
+
+    /*
+    ** Parameters
+    */
+    
+    private String Name;
+    private String[] Class = {"Warrior", "Wizard", "Knight"};
+    private int Level;
+    private int Experience;
+    private int Attack;
+    private int Defense;
+    private int HP;
+        
+    /*
+    ** Methods
+    */
+
+    hero(String p_class, String p_name)
+    {
+
+
+    }
+}
